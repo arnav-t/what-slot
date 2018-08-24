a698b324ef71ddaeaf5d10366209650c283b0c57
arnav-t <avznav@gmail.com>
2018-08-24 18:14:18 +0530

    Updated search functionality

--- search.py
+++ search.py
@@ -1,11 +1,29 @@
 import json, re
 
 dataFileName = 'courses.json'
+slotFileName = 'slots.1.txt'
+
+def slot2Time(slot):
+	with open(slotFileName, 'r') as slotFile:
+		for line in slotFile:
+			if slot == line[:2]:
+				return line[3:].split()
+	return []
 
 def searchData(query):
 	with open(dataFileName, 'r') as dataFile:
 		data = json.load(dataFile)
-	return [ course for course in data if re.search( query, course['Name'], re.IGNORECASE ) ]
+	results = [ course for course in data if re.search( query, course['Name'], re.IGNORECASE ) ]
+	
+	ret = []
+	for course in results:
+		slots = []
+		for slot in course['Data']['Slot'].split(','):
+			slots.extend( slot2Time( slot.strip() ) )
+		course['Data']['Slot'] = slots
+		ret.append(course)
+
+	return ret 
 
 if __name__ == '__main__':
 	print( searchData( input('Search for: ') ) )
